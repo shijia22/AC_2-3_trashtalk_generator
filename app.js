13855454a017a4a71bfd5140ab93dc2166d223d5
shichia22 <shichia0207@gmail.com>
2021-07-10 23:28:16 +0800

    fix: Create Project.

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 // 載入 express 並建構應用程式伺服器
 const express = require('express')
+const exphbs = require('express-handlebars')
 const app = express()
+const port = 3000
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
 
 // 設定首頁路由
 app.get('/', (req, res) => {
-  res.send('hello world')
+  res.render('index')
 })
 
 // 設定 port 3000
 app.listen(3000, () => {
-  console.log('App is running on http://localhost:3000')
+  console.log(`App is running on port ${port}.`)
 })
